81073926a5e1fb2c409e3a79e375e07e2578e502
anacarolsfcabral <aninha_carolcabral@hotmail.com>
2019-12-11 17:06:12 -0300

    Criando projeto, rotas, controller e blade template

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    
+
+    public function viewProdutos () {
+        return view('produtos');
+    }
+
+}
